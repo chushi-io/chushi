bf9b2cc17c3119a2ed42352bde6ef8024bdb3c1a
Robert Wittman <robkwittman@gmail.com>
2024-02-25 12:38:21 -0500

    Move runner logic to module

---
+++ internal/runner/log_adapter.go
@@ -0,0 +1,60 @@
+package runner
+
+import (
+	"connectrpc.com/connect"
+	"context"
+	"crypto/tls"
+	v1 "github.com/chushi-io/chushi/gen/agent/v1"
+	agentv1 "github.com/chushi-io/chushi/gen/agent/v1/agentv1connect"
+	"golang.org/x/net/http2"
+	"net"
+	"net/http"
+	"strings"
+)
+
+type logAdapter struct {
+	Logs   agentv1.LogsClient
+	output [][]byte
+}
+
+func newLogAdapter(grpcUrl string) *logAdapter {
+	logsClient := agentv1.NewLogsClient(newInsecureClient(), grpcUrl, connect.WithGRPC())
+	adapter := &logAdapter{Logs: logsClient}
+	return adapter
+}
+
+func (adapter *logAdapter) Write(p []byte) (n int, err error) {
+	adapter.output = append(adapter.output, p)
+	_, err = adapter.Logs.StreamLogs(context.TODO(), connect.NewRequest(&v1.StreamLogsRequest{Content: string(p)}))
+	if err != nil {
+		return 0, err
+	}
+	return len(p), nil
+}
+
+func (adapter *logAdapter) Flush() error {
+	var lines []string
+	for _, log := range adapter.output {
+		lines = append(lines, string(log))
+	}
+
+	_, err := adapter.Logs.UploadLogs(context.TODO(), connect.NewRequest(&v1.UploadLogsRequest{
+		Content: strings.Join(lines, "\n"),
+	}))
+	return err
+}
+
+func newInsecureClient() *http.Client {
+	return &http.Client{
+		Transport: &http2.Transport{
+			AllowHTTP: true,
+			DialTLS: func(network, addr string, _ *tls.Config) (net.Conn, error) {
+				// If you're also using this client for non-h2c traffic, you may want
+				// to delegate to tls.Dial if the network isn't TCP or the addr isn't
+				// in an allowlist.
+				return net.Dial(network, addr)
+			},
+			// Don't forget timeouts!
+		},
+	}
+}
