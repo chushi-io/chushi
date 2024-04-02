36686c5a8d86820990409c586b3e26990bf1c519
Robert Wittman <robkwittman@gmail.com>
2024-04-01 21:18:04 -0400

    Add claims to JWT

--- internal/grpc/auth.go
+++ internal/grpc/auth.go
@@ -3,17 +3,21 @@ package grpc
 import (
 	"connectrpc.com/connect"
 	"context"
+	"fmt"
 	v1 "github.com/chushi-io/chushi/gen/api/v1"
+	"github.com/chushi-io/chushi/internal/server/config"
 	"github.com/golang-jwt/jwt/v4"
 )
 
 type AuthServer struct {
 	jwtSecret string
+	issuerUrl string
 }
 
-func NewAuthServer(jwtSecret string) *AuthServer {
+func NewAuthServer(conf *config.Config) *AuthServer {
 	return &AuthServer{
-		jwtSecret: jwtSecret,
+		jwtSecret: conf.JwtSecretKey,
+		issuerUrl: conf.IssuerUrl,
 	}
 }
 
@@ -26,6 +30,8 @@ func (a *AuthServer) GenerateRunnerToken(
 			"workspace":    req.Msg.WorkspaceId,
 			"run":          req.Msg.RunId,
 			"organization": req.Msg.OrganizationId,
+			"subject":      fmt.Sprintf("org/%s/workspace/%s", req.Msg.OrganizationId, req.Msg.WorkspaceId),
+			"issuer":       a.issuerUrl,
 		})
 	s, err := t.SignedString([]byte(a.jwtSecret))
 	if err != nil {
--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -7,6 +7,7 @@ type Config struct {
 	JwtSecretKey  string `mapstructure:"jwt_secret_key"`
 	SessionSecret string `mapstructure:"session_secret"`
 	CookieSecret  string `mapstructure:"cookie_secret"`
+	IssuerUrl     string `mapstructure:"issuer_url"`
 }
 
 // Load will parse any environment variables in .env
@@ -21,6 +22,7 @@ func Load() (*Config, error) {
 	v.BindEnv("jwt_secret_key")
 	v.BindEnv("session_secret")
 	v.BindEnv("cookie_secret")
+	v.BindEnv("issuer_url")
 
 	config := &Config{}
 	if err := v.Unmarshal(config); err != nil {
