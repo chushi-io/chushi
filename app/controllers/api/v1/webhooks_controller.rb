4ff4cc3dea92348d5edfc222a31f42cc5addfc03
Robert Wittman <robkwittman@gmail.com>
2024-06-23 20:45:03 -0400

    Disable access token on webhook route

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -1,4 +1,5 @@
-class Api::V1::WebhooksController < ApplicationController
+class Api::V1::WebhooksController < Api::ApiController
+  skip_before_action :verify_access_token
 
   def create
     puts request.body
