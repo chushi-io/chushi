0b8fdc35498968806743ab5e0a0bf8123071050e
Robert Wittman <robkwittman@gmail.com>
2024-10-07 22:59:22 -0400

    Only process outputs if present in state file

--- app/sidekiq/process_state_version_job.rb
+++ app/sidekiq/process_state_version_job.rb
@@ -15,26 +15,28 @@ class ProcessStateVersionJob
     @version.json_state_file.open do |tempfile|
       # Parse all of the outputs
       @parsed_json = ActiveSupport::JSON.decode(File.read(tempfile))
-      @parsed_json["values"]["outputs"].each do |key, output|
-        @output = @version.state_version_outputs.find_by(name: key)
-        if @output
-          # We need to also update state versions?
-        else
-          output_type = output["type"]
-          if output_type.kind_of?(Array)
-            output_type = output["type"].to_json
-          end
+      if @parsed_json["values"].has_key?("outputs")
+        @parsed_json["values"]["outputs"].each do |key, output|
+          @output = @version.state_version_outputs.find_by(name: key)
+          if @output
+            # We need to also update state versions?
+          else
+            output_type = output["type"]
+            if output_type.kind_of?(Array)
+              output_type = output["type"].to_json
+            end
 
-          value = output["value"].to_json
-          if %w[string number].include?(output_type)
-            value = output["value"]
+            value = output["value"].to_json
+            if %w[string number].include?(output_type)
+              value = output["value"]
+            end
+            @output = @version.state_version_outputs.create(
+              sensitive: output["sensitive"],
+              name: key,
+              output_type: output_type,
+              value: value
+            )
           end
-          @output = @version.state_version_outputs.create(
-            sensitive: output["sensitive"],
-            name: key,
-            output_type: output_type,
-            value: value
-          )
         end
       end
 
