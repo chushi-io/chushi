efe230316e40d5aa0c4e65620d0be5dbed9117c2
Robert Wittman <robkwittman@gmail.com>
2024-10-09 17:19:59 -0400

    Fix env parsing

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -37,17 +37,17 @@ Config.setup do |config|
   # with Heroku, but you might want to change it for example for '__' to easy override settings from command line, where
   # using dots in variable names might not be allowed (eg. Bash).
   #
-  # config.env_separator = '.'
+  config.env_separator = '__'
 
   # Ability to process variables names:
   #   * nil  - no change
   #   * :downcase - convert to lower case
   #
-  # config.env_converter = :downcase
+  config.env_converter = :downcase
 
   # Parse numeric values as integers instead of strings.
   #
-  # config.env_parse_values = true
+  config.env_parse_values = true
 
   # Validate presence and type of specific config values. Check https://github.com/dry-rb/dry-validation for details.
   #
