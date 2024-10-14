4a13ce309c4ea485b97a05595fbb76717d6d92f1
Robert Wittman <robkwittman@gmail.com>
2024-10-13 21:06:24 -0400

    Remove Apply job namespace

--- app/sidekiq/apply/apply_errored_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Apply
-  class ApplyErroredJob
-    include Sidekiq::Job
-
-    def perform(*args); end
-  end
-end
--- app/sidekiq/apply/apply_finished_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Apply
-  class ApplyFinishedJob
-    include Sidekiq::Job
-
-    def perform(*args); end
-  end
-end
