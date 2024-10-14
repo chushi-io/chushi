855979a6163c11e3f9d9d8496c9dbe12a88f384c
Robert Wittman <robkwittman@gmail.com>
2024-10-13 23:58:16 -0400

    Refactor module

--- lib/middleware/consider_all_request_json_middleware.rb
+++ lib/middleware/consider_all_request_json_middleware.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-class Middleware::ConsiderAllRequestJsonMiddleware
-  def initialize(app)
-    @app = app
-  end
+module Middleware
+  class ConsiderAllRequestJsonMiddleware
+    def initialize(app)
+      @app = app
+    end
 
-  def call(env)
-    env['CONTENT_TYPE'] = 'application/json' if env['PATH_INFO'].match(%r{\A/api/.*}) && env['CONTENT_TYPE'] == ('application/x-www-form-urlencoded') # if match /api/*
-    @app.call(env)
+    def call(env)
+      env['CONTENT_TYPE'] = 'application/json' if env['PATH_INFO'].match(%r{\A/api/.*}) && env['CONTENT_TYPE'] == ('application/x-www-form-urlencoded') # if match /api/*
+      @app.call(env)
+    end
   end
 end
