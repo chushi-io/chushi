605a3a0ad554cbd2f84daf6b6984ed12e58f789e
Robert Wittman <robkwittman@gmail.com>
2024-10-13 22:59:07 -0400

    Fix filepath

--- lib/middleware/consider_all_request_json_middleware.rb
+++ lib/middleware/consider_all_request_json_middleware.rb
@@ -6,7 +6,11 @@ class ConsiderAllRequestJsonMiddleware
   end
 
   def call(env)
-    env['CONTENT_TYPE'] = 'application/json' if env['CONTENT_TYPE'] == 'application/x-www-form-urlencoded'
+    if env['PATH_INFO'].match(/\A\/api\/.*/) # if match /api/*
+      if env["CONTENT_TYPE"] == 'application/x-www-form-urlencoded'
+        env["CONTENT_TYPE"] = 'application/json'
+      end
+    end
     @app.call(env)
   end
 end
