838f47bd3806740983d434b1103fd8aac3c3da51
Robert Wittman <robkwittman@gmail.com>
2024-10-20 14:50:25 -0400

    Remove sidekiq flag

--- spec/requests/api/v2/runs_controller_spec.rb
+++ spec/requests/api/v2/runs_controller_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'sidekiq/testing'
+# require 'sidekiq/testing'
 
 describe Api::V2::RunsController do
   include JSONAPI::Deserialization
-  Sidekiq::Testing.disable!
+  # Sidekiq::Testing.disable!
 
   context 'when a user is in the workspace "admin" team' do
     organization = Fabricate(:organization)
