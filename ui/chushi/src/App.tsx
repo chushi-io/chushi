a9e5369d5b410cbd6819b81ec5267d0c08edf839
Robert Wittman <robkwittman@gmail.com>
2024-10-23 09:00:34 -0400

    Fix login flow

--- ui/chushi/src/App.tsx
+++ ui/chushi/src/App.tsx
@@ -1,20 +1,25 @@
 import './App.css'
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
-import {AuthProvider, useAuth} from "react-oidc-context";
+import {AuthProvider, hasAuthParams, useAuth} from "react-oidc-context";
 import Router from "./Router.tsx";
+import {User, WebStorageStateStore} from "oidc-client-ts";
+import {useEffect, useState} from "react";
 
 
 // let oidcConfigLoaded = false
 let oidcConfig = {
   authority: "https://caring-foxhound-whole.ngrok-free.app",
   client_id: "c-grvyKTI0gxf1SvvVoHs7hKGTmYoKDygEI7yiljdSM",
-  redirect_uri: location.protocol + '//' + location.host + "/app"
+  redirect_uri: location.protocol + '//' + location.host + "/app",
+  userStore: new WebStorageStateStore({ store: window.localStorage }),
 }
 
 export default function App() {
   return <MantineProvider>
-    <AuthProvider {...oidcConfig}>
+    <AuthProvider {...oidcConfig} onSigninCallback={(_user: User | void) => {
+      window.history.replaceState({}, document.title, window.location.pathname)
+    }}>
       <Main />
     </AuthProvider>
   </MantineProvider>
@@ -22,6 +27,19 @@ export default function App() {
 
 const Main = () => {
     const auth = useAuth();
+
+    const [hasTriedSignin, setHasTriedSignin] = useState(false);
+
+    // automatically sign-in
+    useEffect(() => {
+      if (!hasAuthParams() &&
+        !auth.isAuthenticated && !auth.activeNavigator && !auth.isLoading &&
+        !hasTriedSignin
+      ) {
+        auth.signinRedirect();
+        setHasTriedSignin(true);
+      }
+    }, [auth, hasTriedSignin]);
 
     switch (auth.activeNavigator) {
       case "signinSilent":
