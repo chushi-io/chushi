dc46f27e7805ea1459229adabdd2f3dab48f16ea
Robert Wittman <robkwittman@gmail.com>
2024-11-04 21:29:38 -0500

    Stub out controller methods for Oauth tokens

--- app/controllers/api/v2/oauth_tokens_controller.rb
+++ app/controllers/api/v2/oauth_tokens_controller.rb
@@ -1,17 +1,28 @@
 class Api::V2::OauthTokensController < ApplicationController
   def index
+    @client = OauthClient.find_by(external_id: params[:id])
+    authorize! @client.organization, to: :read?
 
+    @tokens = @client.oauth_tokens
+    render json: ::OauthTokenSerializer.new(@tokens, {}).serializable_hash
   end
 
   def show
+    @token = OauthToken.find_by(external_id: params[:id])
+    unless @token
+      skip_verify_authorized!
+      head :not_found and return
+    end
 
+    authorize! @token.oauth_client.organization, to: :read?
+    render json: ::OauthTokenSerializer.new(@token, {}).serializable_hash
   end
 
   def update
-
+    head :not_implemented
   end
 
   def destroy
-
+    head :not_implemented
   end
 end
