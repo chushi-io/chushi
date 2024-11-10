fc0d8d69429a861e666d86ffc5ff57cf2c40ce5a
Robert Wittman <robkwittman@gmail.com>
2024-11-09 21:22:02 -0500

    Formatting

--- app/serializers/openbao_cluster_serializer.rb
+++ app/serializers/openbao_cluster_serializer.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class OpenbaoClusterSerializer < ApplicationSerializer
-  set_type "openbao-clusters"
+  set_type 'openbao-clusters'
 
   attribute :name
   attribute :size
@@ -17,5 +19,4 @@ class OpenbaoClusterSerializer < ApplicationSerializer
   belongs_to :organization, serializer: OrganizationSerializer, id_method_name: :name, &:organization
   belongs_to :project, serializer: ProjectSerializer, id_method_name: :external_id, &:project
   belongs_to :virtual_network, serializer: VirtualNetwork, id_method_name: :external_id, &:virtual_network
-
 end
