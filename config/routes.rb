de50509da5f6a60f615ccb2d05eb02df48516981
Robert Wittman <robkwittman@gmail.com>
2024-12-02 20:49:04 -0500

    Rubocop fixes

--- config/routes.rb
+++ config/routes.rb
@@ -288,5 +288,5 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  root :to => redirect('/app/organizations')
+  root to: redirect('/app/organizations')
 end
